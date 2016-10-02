54048d3a19e09b1bfcb36911f058f78a95c8e958
gaykov <sdgaykov@gmail.com>
2016-10-02 12:37:28 +0300

    ui-elements started

---
+++ src/app/ui-elements/notifications/notifications.component.ts
@@ -0,0 +1,11 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: '[ui-components]',
+  template: require('./notifications.template.html'),
+  encapsulation: ViewEncapsulation.None,
+  styles: [require('./notifications.style.scss')]
+})
+export class Notifications {
+}
+
--- src/app/ui-elements/ui-elements.module.ts
+++ src/app/ui-elements/ui-elements.module.ts
@@ -1,19 +1,33 @@
+import 'messenger/build/js/messenger.js';
+import 'jquery-ui/ui/sortable.js';
+import 'jquery.nestable/jquery.nestable.js'
+
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AlertModule, TooltipModule, ButtonsModule, DropdownModule } from 'ng2-bootstrap/ng2-bootstrap'
+import { AlertModule, TooltipModule, ButtonsModule, DropdownModule, TabsModule, AccordionModule } from 'ng2-bootstrap/ng2-bootstrap'
 import { ModalModule } from 'ng2-modal'
 
 import {Components} from './components/components.component'
 import {WidgetModule} from '../layout/widget/widget.module';
 import {ModalComponent} from './components/modal-window/modal.component';
 import {Buttons} from './buttons/buttons.component';
+import {Notifications} from './notifications/notifications.component';
+import {Icons} from './icons/icons.component';
+import {TabsAccordion} from './tabs-accordion/tabs-accordion.component';
+import {ListGroups} from './list-groups/list-groups.component';
+
+import {MessengerDemo} from './notifications/messenger/messenger';
 
 export const routes = [
   {path: '', redirectTo: 'components', pathMatch: 'full'},
   {path: 'components', component: Components},
   {path: 'buttons', component: Buttons},
+  {path: 'notifications', component: Notifications},
+  {path: 'icons', component: Icons},
+  {path: 'tabs-accordion', component: TabsAccordion},
+  {path: 'list-groups', component: ListGroups},
 ];
 
 @NgModule({
@@ -21,7 +35,12 @@ export const routes = [
     // Components / Directives/ Pipes
     Components,
     Buttons,
-    ModalComponent
+    ModalComponent,
+    Notifications,
+    MessengerDemo,
+    Icons,
+    TabsAccordion,
+    ListGroups
   ],
   imports: [
     CommonModule,
@@ -32,7 +51,9 @@ export const routes = [
     TooltipModule,
     ModalModule,
     ButtonsModule,
-    DropdownModule
+    DropdownModule,
+    TabsModule,
+    AccordionModule
   ]
 })
 export default class UiElementsModule {
