5acedd7bd8c2ddbd3d90b930bf8e37d2fb95209c
shoudaos <shoudaos92@gmail.com>
2016-10-05 19:50:14 +0300

    feat(form-elements): add inputmask

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -13,6 +13,9 @@ import 'bootstrap-select/dist/js/bootstrap-select.js';
 import 'parsleyjs';
 import 'bootstrap-application-wizard/src/bootstrap-wizard.js';
 import 'twitter-bootstrap-wizard/jquery.bootstrap.wizard.js';
+import 'jasny-bootstrap/docs/assets/js/vendor/holder.js';
+import 'jasny-bootstrap/js/fileinput.js';
+import 'jasny-bootstrap/js/inputmask.js';
 // import 'ng2-datetime/src/vendor/bootstrap-datepicker/bootstrap-datepicker.min.js';
 // import 'ng2-datetime/src/vendor/bootstrap-timepicker/bootstrap-timepicker.min.js';
 
