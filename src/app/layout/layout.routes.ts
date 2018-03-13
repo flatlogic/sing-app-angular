6465545bf2b83be0af23e51e17e52bdc54a4bd2c
shoudaos <shoudaos92@gmail.com>
2018-03-13 18:57:49 +0300

    Release/angular/4.1.0 (#56)
    * new(product-grid): product-grid page
    
    * new(product-details): product-details page (#28)
    
    * new(product-details): product-details page
    
    * fix(product-details): rating added & some style fixes
    
    * fix(product-details): pr fixes
    
    * feat(package-page): add angularjs section
    
    * feat(layout): add content footer
    
    #25
    
    * fix(products-grid): add adaptivity (#35)
    
    * feat(product-detail): broken into components and added adaptivity (#36)
    
    * feat: rename product pages, various style adjustments
    
    * Pr/angular/refactor product (#48)
    
    * fix(product grid): refactor styles
    
    * fix(product details): refactor styles, add payment icons
    
    * remove libs' version prefixes

--- src/app/layout/layout.routes.ts
+++ src/app/layout/layout.routes.ts
@@ -2,12 +2,13 @@ import { Routes, RouterModule }  from '@angular/router';
 import { Layout } from './layout.component';
 // noinspection TypeScriptValidateTypes
 const routes: Routes = [
-  { path: '', component: Layout, children: [
+    { path: '', component: Layout, children: [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'dashboard', loadChildren: '../dashboard/dashboard.module#DashboardModule' },
     { path: 'inbox', loadChildren: '../inbox/inbox.module#InboxModule' },
     { path: 'charts', loadChildren: '../charts/charts.module#ChartsModule' },
     { path: 'profile', loadChildren: '../profile/profile.module#ProfileModule' },
+    { path: 'ecommerce', loadChildren: '../ecommerce/ecommerce.module#EcommerceModule' },
     { path: 'forms', loadChildren: '../forms/forms.module#FormModule' },
     { path: 'ui', loadChildren: '../ui-elements/ui-elements.module#UiElementsModule' },
     { path: 'extra', loadChildren: '../extra/extra.module#ExtraModule' },
